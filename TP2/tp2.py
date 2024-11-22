0c3b92a1ee96193d9b039db3847d6364615146b8
GON <longogonzalo.g@gmail.com>
2024-11-22 08:37:28 -0300

    Ejercicio 1 terminado, falta decorar

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -1,7 +1,9 @@
+# Importamos librerias necesarias
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+# Definimos funcion para simplificar codigo de visualizaciones
 def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, colorbar=False, ticks=False):
     if new_fig:
         plt.figure()
@@ -33,15 +35,40 @@ imshow(gray_blur)
 # Detectamos los circulos y los dibujamos en una nueva imagen binaria
 circles = cv2.HoughCircles(gray_blur,cv2.HOUGH_GRADIENT, 1, 200, param1=50, param2=50, minRadius=130, maxRadius=200)  # Circulos chicos
 
+
+
 circles = np.uint16(np.around(circles))
 monedas_bin = np.zeros_like(gray)
-for i in circles[0,:]:
-    cv2.circle(monedas_bin, (i[0],i[1]), i[2], 1, -1)   # draw the outer circle
-    cv2.circle(monedas_bin, (i[0],i[1]), 2, 1, -1)      # draw the center of the circle
+tipos_monedas = np.zeros_like(img)
+cant_10c = 0
+cant_50c = 0
+cant_1p = 0
+for c in circles[0,:]:
+    cv2.circle(monedas_bin, (c[0],c[1]), c[2], 1, -1)
+    if c[2] > 170 and c[2] < 190: #Grandes
+        cv2.circle(tipos_monedas, (c[0],c[1]), c[2], (255,0,0), -1)
+        cant_50c += 1
+    elif c[2] > 150 and c[2] < 170: #Medianas
+        cv2.circle(tipos_monedas, (c[0],c[1]), c[2], (0,255,0), -1)
+        cant_1p += 1  
+    elif c[2] > 120 and c[2] < 150: #Chicas
+        cv2.circle(tipos_monedas, (c[0],c[1]), c[2], (0,0,255), -1)  
+        cant_10c += 1
 
+print("Tenemos un total de ", cant_10c + cant_50c + cant_1p, " monedas" )
+print("Cantidad de monedas segun su tipo:")
+print("10 centavos : ", cant_10c)
+print("50 centavos : ", cant_50c)
+print("1 peso : ", cant_1p)
+imshow(img)
+imshow(tipos_monedas)
 # Visualizamos el resultado
 plt.imshow(monedas_bin, cmap="gray")
 plt.show()
+
+
+
+
 
 # Dilatamos los circulos para que cubran cada moneda o podemos hacerlo luego de
 # "tapar" las monedas
@@ -50,7 +77,7 @@ kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (L, L) )
 monedas_bin_dil = cv2.dilate(monedas_bin, kernel, iterations=1)
 
 ax1 = plt.subplot(121); imshow(monedas_bin, new_fig=False, title="Monedas original")
-plt.subplot(122, sharex=ax1, sharey=ax1); imshow(Fd, new_fig=False, title="Monedas dilatadas")
+plt.subplot(122, sharex=ax1, sharey=ax1); imshow(monedas_bin_dil, new_fig=False, title="Monedas dilatadas")
 plt.show(block=False)
 
 # Copiamos la imagen de grises y eliminamos las monedas pintando de negro o gris oscuro
@@ -61,7 +88,7 @@ imshow(gray_dados)
 
 
 # Definir el umbral
-threshold = 180
+threshold = 185
 
 # Aplicar la función threshold
 _, dados_bin = cv2.threshold(gray_dados, threshold, 1, cv2.THRESH_BINARY)
@@ -80,12 +107,42 @@ for i in range(num_labels):
 
 imshow(dados_bin)
 
+
 # Dilatamos los dados
 L = 80 # Con 70 esta bien pero pongo de mas ya que los circulos podrian ser mas grandes
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (L, L))
-Acl = cv2.morphologyEx(dados_bin, cv2.MORPH_CLOSE, kernel)
-imshow(Acl)
+dados_bin = cv2.morphologyEx(dados_bin, cv2.MORPH_CLOSE, kernel)
+#dados_bin2 = cv2.morphologyEx(dados_bin2, cv2.MORPH_OPEN, kernel) # Queda redondeado, no esta mal, pero no hace falta tanto detalle
+imshow(dados_bin)
 
-# TENGO 2 MASCARAS;
-    #MONEDAS : monedas_bin
-    #DADOS : dados_bin
+# Pensé 2 alternativas: 
+    #+ dejar los circulos de los dados en la mascara y reconstruirla
+    # con dilatacion, esto hace que se achiquen los circulos, pero se mantienen visibles
+
+    # Elimnar los circulos de los dados, se obtienen cuadrados, aplicar la mascara
+    # a la imagen original en escala de grises y obtener los circulos mas definidos
+
+# OPCION 1
+# Obtengo los circulos pequeños de la imagen en escala de grises y los dibujo en la imagen binaria
+# de esta forma obtengo circulos mas definidos que los obtenidos con la umbralizacion
+circles = cv2.HoughCircles(gray_blur,cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=50, minRadius=10, maxRadius=40)  # Circulos chicos
+circles = np.uint16(np.around(circles))
+
+for c in circles[0,:]:
+    cv2.circle(dados_bin, (c[0],c[1]), c[2], 0, -1)
+
+imshow(dados_bin)
+
+connectivity = 8
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(dados_bin, connectivity, cv2.CV_32S)  
+imshow(labels)
+
+# Analizar por separado y calcular el numero de cada dado, mostrar el dado
+# y el titulo con el numero del dado. Utilizar houghcircle para cada recorte de imagen
+# con un dado, con eso ya resuelvo todo, tengo todo listo.
+
+# Creatividad. Crear !!!
+# Agregar mas comentarios, dividir en funciones cada modulo o respuesta del enunciado
+# agregar parametros para activar o desactivar la visualizacion de las imagenes paso a paso
+# Hacer imagenes piolas para mostrar la clasificacion de monedas coloreadas y dados
+# con el titulo con el valor del mismo
